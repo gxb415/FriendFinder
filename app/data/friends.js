471e1b73b71de82ec4836e3511794836ccb8dc69
Gina <gxb415@gmail.com>
2018-02-22 09:17:53 -0800

    changes to data

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,16 +18,16 @@ var friends = [{
         "name": "Patrick Star",
         "photo": "https://res.cloudinary.com/teepublic/image/private/s--G-sEUdGD--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1446161353/production/designs/71233_0.jpg",
         "scores": [
-            5,
-            1,
             4,
             4,
-            5,
-            1,
-            2,
-            5,
             4,
-            1
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
         ]
     },
 
@@ -35,32 +35,32 @@ var friends = [{
         "name": "Squidward",
         "photo": "https://ih1.redbubble.net/image.211332711.7020/flat,800x800,075,f.u3.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
-            5,
-            4,
-            1
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
         ]
     },
     {
         "name": "Sandy Cheeks",
         "photo": "https://vignette.wikia.nocookie.net/spongebob/images/a/a0/Sandy_Cheeks.svg/revision/latest?cb=20100920201409",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     },
     {
@@ -68,30 +68,30 @@ var friends = [{
         "photo": "https://vignette.wikia.nocookie.net/theultimatepongebob/images/a/a6/SpongeBob_with_logo.jpg/revision/latest?cb=20150629131616",
         "scores": [
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     },
     {
         "name": "Mr. Krabs",
         "photo": "https://vignette.wikia.nocookie.net/spongebob/images/9/9f/Krabs_artwork.PNG/revision/latest?cb=20171223013253",
         "scores": [
-            5,
             1,
-            4,
-            4,
-            5,
             1,
-            2,
-            5,
-            4,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
     }
